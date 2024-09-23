3e7a8b1fe4d3def3a0344e4c891bea753ed95ff0
Oleg Ivanov <o.ivanov@innopolis.ru>
2024-09-23 17:57:43 +0300

    Add lite train augmentations

--- uncertainty_isic/pipeline/transforms/augmentation.py
+++ uncertainty_isic/pipeline/transforms/augmentation.py
@@ -3,7 +3,7 @@ from albumentations.pytorch import ToTensorV2
 
 
 def get_transforms(img_size: int):
-    transforms = {'train' :  A.Compose([
+    transforms = {'train_old' :  A.Compose([
         A.Resize(img_size, img_size),
         A.RandomRotate90(p=0.5),
         A.Flip(p=0.5),
@@ -39,6 +39,14 @@ def get_transforms(img_size: int):
         ]),
 
         'tta' : A.Compose([
+            A.RandomResizedCrop(height=img_size, width=img_size, scale=(0.8, 1.0), ratio=(0.75, 1.33), p=1.0),
+            A.Resize(height=img_size, width=img_size, p=1.0),
+            A.Normalize(mean=[0.485, 0.456, 0.406], 
+                        std=[0.229, 0.224, 0.225], 
+                        max_pixel_value=255.0, p=1)
+        ]),
+
+        'train' : A.Compose([
             A.RandomResizedCrop(height=img_size, width=img_size, scale=(0.8, 1.0), ratio=(0.75, 1.33), p=1.0),
             A.Resize(height=img_size, width=img_size, p=1.0),
             A.Normalize(mean=[0.485, 0.456, 0.406], 
